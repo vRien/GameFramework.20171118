49b164f6b3eb810b915879e1f3f5e08de09eed14
kuna3844 <kuna3844@naver.com>
2018-11-27 17:06:24 +0900

    PP17.PlayState

---
+++ PP17.PlayState/GameObject.cpp
@@ -0,0 +1,18 @@
+#include "GameObject.h"
+
+void GameObject::load(int x, int y, int width, int height,
+	std::string textureID)
+{
+	m_x = x;
+	m_y = y;
+	m_width = width;
+	m_height = height;
+	m_textureID = textureID;
+	m_currentRow = 1;
+	m_currentFrame = 1;
+}
+
+void GameObject::update()
+{
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 5));
+}
