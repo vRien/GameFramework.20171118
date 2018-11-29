ad5605bb93dada06836f94db63533be1ea6bf76a
김선우 <kuda3844@naver.com>
2018-11-29 18:20:54 +0900

    PP19.GameOverState

---
+++ PP19.GameOverState/GameOverState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include"GameObject.h"
+#include"GameState.h"
+
+class GameObject;
+
+class GameOverState : public GameState
+{
+public:
+	virtual void update();
+	virtual void render();
+	virtual bool onEnter();
+	virtual bool onExit();
+	virtual std::string getStateID() const { return s_gameOverID; }
+private:
+	static void s_gameOverToMain();
+	static void s_restartPlay();
+	static const std::string s_gameOverID;
+	std::vector<GameObject*> m_gameObjects;
+};
---
+++ PP19.GameOverState/SDLGameObject.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "GameObject.h"
+#include "TextureManager.h"
+#include "Vector2D.h"
+
+class SDLGameObject : public GameObject
+{
+public:
+	SDLGameObject(const LoaderParams* pParams);
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+
+	Vector2D& getPosition() { return m_position; }
+	int getWidth() { return m_width; }
+	int getHeight() { return m_height; }
+	int m_numFrames;
+protected:
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+
+	int m_width;
+	int m_height;
+	int m_currentRow;
+	int m_currentFrame;
+	std::string m_textureID;
+};
