169999144acf1caa6946ca8da095dce1dd3d9b18
권기석 <soorison@naver.com>
2018-12-04 13:18:19 +0900

    no message

--- PP19.GameOverState/Enemy.cpp
+++ PP19.GameOverState/Enemy.cpp
@@ -6,7 +6,7 @@
 Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams)
 {
 	m_velocity.setY(2);
-	m_velocity.setX(0.001);
+	m_velocity.setX(1);
 }
 
 void Enemy::draw()
@@ -18,9 +18,20 @@ void Enemy::update()
 {
 	m_currentFrame = int(((SDL_GetTicks() / 100) % 5));
 	if (m_position.getY() < 0) {
+		m_velocity.setX(-2);
+		m_velocity.setY(2);
+	}
+	else if (m_position.getX() < 0)
+	{
+		m_velocity.setX(2);
 		m_velocity.setY(2);
 	}
 	else if (m_position.getY() > 400) {
+		m_velocity.setX(2);
+		m_velocity.setY(-2);
+	}
+	else if (m_position.getX() > 500) {
+		m_velocity.setX(-2);
 		m_velocity.setY(-2);
 	}
 	SDLGameObject::update();
